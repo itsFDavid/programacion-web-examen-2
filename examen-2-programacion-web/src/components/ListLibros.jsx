ff7ebbc79a9cb9aa192d5cc023efc0d954833f7d
itsFDavid <FDavid04@icloud.com>
2025-05-06 08:37:07 -0600

    add search functionality to ListLibros component

--- examen-2-programacion-web/src/components/ListLibros.jsx
+++ examen-2-programacion-web/src/components/ListLibros.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
-import { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 import Libro from './Libro';
 
 function ListLibros() {
   const { libros } = useContext(AppContext);
+  const [busqueda, setBusqueda] = useState('');
+
+
+  const librosFiltrados = libros.filter(libro =>
+    libro.titulo.toLowerCase().includes(busqueda.toLowerCase())
+  );
 
   return (
     <>
-    <h1>Lista de Libros</h1>
-    <div className='listLibros'>
-      <ul>
-        {libros.map((libro, index) => (
-          <Libro libro={libro} key={index}/>
-        ))}
-      </ul>
-    </div>
+      <h1>Lista de Libros</h1>
+      <input
+        type="text"
+        placeholder="Buscar por título..."
+        value={busqueda}
+        onChange={(e) => setBusqueda(e.target.value)}
+      />
+      <div className='listLibros'>
+        <ul>
+          {librosFiltrados.map((libro, index) => (
+            <Libro libro={libro} key={index} />
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
