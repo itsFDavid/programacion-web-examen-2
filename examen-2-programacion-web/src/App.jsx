de144ae2db91628749d0e2095fb488b46c6bb8c6
itsFDavid <FDavid04@icloud.com>
2025-05-06 08:15:25 -0600

    init repo

---
+++ examen-2-programacion-web/src/App.jsx
@@ -0,0 +1,16 @@
+// filepath: /Users/david/Desktop/Semestre-6/programacion-web/examen-2/examen-2-programacion-web/src/App.jsx
+import './App.css';
+import ListLibros from './components/ListLibros';
+
+function App() {
+  return (
+    <>
+      <h1>Examen 2</h1>
+      <main className='main'>
+        <ListLibros />
+      </main>
+    </>
+  );
+}
+
+export default App;
