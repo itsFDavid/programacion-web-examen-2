8f7be2867e30592d099ef0dcb3e130779682d79f
itsFDavid <FDavid04@icloud.com>
2025-05-06 08:36:54 -0600

    add form_agregar styles and integrate FormularioLibro component

--- examen-2-programacion-web/src/App.jsx
+++ examen-2-programacion-web/src/App.jsx
@@ -1,5 +1,6 @@
 // filepath: /Users/david/Desktop/Semestre-6/programacion-web/examen-2/examen-2-programacion-web/src/App.jsx
 import './App.css';
+import FormularioLibro from './components/FormularioLibro';
 import ListLibros from './components/ListLibros';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <h1>Examen 2</h1>
       <main className='main'>
+        <FormularioLibro />
         <ListLibros />
       </main>
     </>
