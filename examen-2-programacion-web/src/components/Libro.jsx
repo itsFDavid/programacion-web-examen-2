6eec83a5eca428cb2789faaf6ea11f01492c7336
itsFDavid <FDavid04@icloud.com>
2025-05-06 08:37:02 -0600

    add delete functionality and edit toggle to Libro component

--- examen-2-programacion-web/src/components/Libro.jsx
+++ examen-2-programacion-web/src/components/Libro.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { useState } from 'react';
+import { AppContext } from '../context/AppContext';
+import { useContext } from 'react';
 
 const Libro = ({libro}) => {
-  console.log(libro);
+
   const { titulo, autor, genero, fecha_publicacion, leido } = libro;
   const fecha = new Date(fecha_publicacion);
   const fechaFormateada = `${fecha.getDate()}/${fecha.getMonth() + 1}/${fecha.getFullYear()}`;
   const leidoTexto = leido ? 'Sí' : 'No';
+
+  const [editando, setEditando] = useState(false);
+  const { deleteLibro } = useContext (AppContext);
+
+  const handleDelete = (libro) => {
+    if (window.confirm(`¿Estás seguro de que quieres eliminar el libro "${libro.titulo}"?`)) {
+      deleteLibro(libro.titulo);
+    }
+  }
+
   return (
     <div className='libro'>
       <h2>{titulo}</h2>
@@ -13,7 +26,14 @@ const Libro = ({libro}) => {
       <p>Género: {genero}</p>
       <p>Fecha de publicación: {fechaFormateada}</p>
       <p>Leído: {leidoTexto}</p>
+      <button onClick={() => setEditando(!editando)}>
+        {editando ? 'Cancelar' : 'Editar'}
+      </button>
+      <button onClick={() => handleDelete(libro)}>
+        Eliminar libro
+      </button>
       <hr />
+
     </div>
   )
 }
