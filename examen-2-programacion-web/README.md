fff49c2622b1d1122091f0c083223a5f5afcc626
itsFDavid <FDavid04@icloud.com>
2025-05-06 09:40:23 -0600

    refactor ListLibros component to include a checkbox for filtering read books; update styles for search input and labels in App.css

--- examen-2-programacion-web/README.md
+++ examen-2-programacion-web/README.md
@@ -1,12 +1,32 @@
-# React + Vite
+# Examen unidad 2 - Programacion Web
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Francisco David Colin Lira
 
-Currently, two official plugins are available:
+Grupo: 3602
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Pasos
 
-## Expanding the ESLint configuration
+1. Clonar el repositorio
 
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+```bash
+git clone https://github.com/itsFDavid/programacion-web-examen-2.git
+```
+
+2. Entrar a la carpeta del examen
+
+3. Instalar las dependencias
+
+```bash
+pnpm install
+
+npm intall
+```
+
+4. Ejecutar el proyecto en modo dev
+
+```bash
+# escoje la opcion
+pnpm run dev
+
+npm run dev
+```
--- examen-2-programacion-web/src/components/ListLibros.jsx
+++ examen-2-programacion-web/src/components/ListLibros.jsx
@@ -5,32 +5,50 @@ import Libro from './Libro';
 function ListLibros() {
   const { libros } = useContext(AppContext);
   const [busqueda, setBusqueda] = useState('');
+  const [mostrarSoloLeidos, setMostrarSoloLeidos] = useState(false);
 
 
-  const librosFiltrados = libros.filter(libro =>
-    libro.titulo.toLowerCase().includes(busqueda.toLowerCase())
-    || libro.autor.toLowerCase().includes(busqueda.toLowerCase())
-    || libro.genero.toLowerCase().includes(busqueda.toLowerCase())
-  );
+  const librosFiltrados = libros
+    .filter(libro => (mostrarSoloLeidos ? libro.leido : true))
+    .filter(libro =>
+      libro.titulo.toLowerCase().includes(busqueda.toLowerCase())
+      || libro.autor.toLowerCase().includes(busqueda.toLowerCase())
+      || libro.genero.toLowerCase().includes(busqueda.toLowerCase())
+    );
 
+
+
+
+  
   // console.log(librosFiltrados);
 
   return (
     <>
       <h1>Lista de Libros</h1>
-      <input
-        type="text"
-        placeholder="Buscar por título, autor, genero..."
-        value={busqueda}
-        onChange={(e) => setBusqueda(e.target.value)}
-        className='input_busqueda'
-      />
+      <div className='buscador'>
+        <input
+          type="text"
+          placeholder="Buscar por título, autor, genero..."
+          value={busqueda}
+          onChange={(e) => setBusqueda(e.target.value)}
+          className='input_busqueda'
+        />
+        <label htmlFor="soloLeidos" className='label_leidos'>
+          {mostrarSoloLeidos ? 'Mostrar todos' : 'Mostrar solo leídos'}
+          <input
+            type="checkbox" 
+            onChange={() => setMostrarSoloLeidos(!mostrarSoloLeidos)}>
+          </input>
+        </label>
+      </div>
       <div className='listLibros'>
         <ul>
-          {librosFiltrados.length != 0 ? librosFiltrados.map((libro, index) => (
-            <Libro libro={libro} key={index} />
-          )):
-          <p>No hay libros con ese titulo, autor, genero.</p>
+          {
+            librosFiltrados.length != 0 ? 
+            librosFiltrados.map((libro, index) => (
+              <Libro libro={libro} key={index} />
+            )):
+              <p>No hay libros con ese titulo, autor, genero.</p>
           }
         </ul>
       </div>
